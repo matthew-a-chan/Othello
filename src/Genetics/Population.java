11585bf8d4dbdac5dce5b9cac85121241e9454c7
platy23 <crashingturtle22@gmail.com>
2017-12-06 09:55:50 -0800

    Made Da Jeans

---
+++ src/Genetics/Population.java
@@ -0,0 +1,19 @@
+/**
+ * @author Stephen C.
+ */
+package Genetics;
+
+/**
+ * @author Stephen C.
+ *
+ */
+public class Population {
+	
+	final int populationSize=10;
+	final double mutationRate=0.005;
+	
+	public Population() {
+		
+		
+	}
+}
