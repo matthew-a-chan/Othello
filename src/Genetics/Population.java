614eaa26784900f651316c6191e7a1105e94bf59
platy23 <crashingturtle22@gmail.com>
2017-12-22 14:08:35 -0800

    I dunno really...
    Added functionality for population to make the txt files for a new
    generation but doesn't actually fill the txt files... just makes them...
    but it probably shouldn't belong in population tho--should belong
    somewhere else
    
    Other than that I sorta forgot what I did
    
    TODO: 
    -crossovers/mutations (createing new generations)
    -somehow giving individuals the ability to rank themselves
    -Creating new generations based off old ones
    
    Note:
    This should deal with JUST the files. another branch will deal with
    getting the individuals work with the game.
    
    HF :)

--- src/Genetics/Population.java
+++ src/Genetics/Population.java
@@ -3,6 +3,13 @@
  */
 package Genetics;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * @author Stephen C.
  *
@@ -10,10 +17,46 @@ package Genetics;
 public class Population {
 	
 	final int populationSize=10;
-	final double mutationRate=0.005;
+	final double mutationRate=0.09;
+	int genNumber=0;
+	
+	Individual[] population;
+	
+	public static void main(String[] args) {
+		new Population();
+	}
 	
 	public Population() {
 		
+		System.out.println(System.getProperty("user.home")+File.separator+"Desktop"+File.separator+"AI"+File.separator+"DATA");
+		File data=new File(System.getProperty("user.home")+File.separator+"Desktop"+File.separator+"AI"+File.separator+"DATA");
 		
+		newGen();
 	}
+	
+	
+	
+	public void newGen() {
+		population=new Individual[populationSize];
+		File genFolder=new File(System.getProperty("user.home")+File.separator+"Desktop"+File.separator+"AI"+File.separator+"GEN"+genNumber);
+		genFolder.mkdir();
+		for(int i=0;i<populationSize;i++) {
+			File a=new File(System.getProperty("user.home")+File.separator+"Desktop"+File.separator+
+					"AI"+File.separator+"GEN"+genNumber+File.separator+"GEN"+genNumber+"-IND"+i);
+			FileWriter aw;
+			try {
+				aw = new FileWriter(a);
+				aw.write("HELLO");
+				aw.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println(a.getAbsolutePath());
+			population[i]=new Individual(a);
+		}
+		
+		genNumber++;
+	}
+	
+	
 }
