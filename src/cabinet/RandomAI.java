5fb39f2d5f1709dd4f644be8d4e5618acd8e2c3b
platy23 <platypus923@gmail.com>
2018-01-03 00:27:20 -0600

    Changed Random AI to make it more consistent

--- src/cabinet/RandomAI.java
+++ src/cabinet/RandomAI.java
@@ -69,9 +69,7 @@ public class RandomAI extends Player{
 	 */
 	@Override
 	public void makeMove( GameState gs, List<Move> m ){
-		for(int i=0;i<10000000;i++) {
-			Math.random();
-		}
+		try{Thread.sleep(250);}catch(Exception e) {};
 		if(gs.getValidMoves().size()>0) {
 			m.add(gs.getValidMoves().get((int)(Math.random()*gs.getValidMoves().size())));
 		}
